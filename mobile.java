35b6926bee4948756cc8d7714682c6de8a7eceac
Naiba Sree P <112084996+NaibaSree@users.noreply.github.com>
2025-04-04 14:36:33 +0530

    Create mobile.java

---
+++ mobile.java
@@ -0,0 +1,14 @@
+package Collection;
+import java.util.*;
+public class mobile {
+	String brand;
+	String model;
+	int price;
+	int RAM;
+  mobile(String brand, String model,int price, int RAM){
+	  this.brand=brand;
+	  this.model=model;
+	  this.price=price;
+	  this.RAM=RAM;
+  }
+}

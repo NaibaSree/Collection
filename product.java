08a78c43cbb424936693c6b72f631f47753fa51e
Naiba Sree P <112084996+NaibaSree@users.noreply.github.com>
2025-04-04 14:37:25 +0530

    Create product.java

---
+++ product.java
@@ -0,0 +1,14 @@
+package Collection;
+
+public class product {
+	int p_id,price,quality;
+	String p_name,brand,manufacturer;
+	product(int p_id, String p_name,int price,String brand, String manufacturer,int quality){
+		this.p_id=p_id;
+		this.p_name=p_name;
+		this.price=price;
+		this.brand=brand;
+		this.manufacturer=manufacturer;
+		this.quality=quality;
+	}
+}

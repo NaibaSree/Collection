5ccee76671a49f7a930e41bd5a41d9e6576e72e5
Naiba Sree P <112084996+NaibaSree@users.noreply.github.com>
2025-04-04 14:34:50 +0530

    Create HashMap1.java

---
+++ HashMap1.java
@@ -0,0 +1,19 @@
+package Collection;
+
+import java.lang.Integer;
+
+import java.util.HashMap;
+
+public class HashMap1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		HashMap<Integer,String> map=new HashMap<Integer,String>();
+	    map.put(1,"Apple");
+	    map.put(2,"Kiwi");
+	    map.put(3,"Banana");
+	    System.out.println(map);
+	}
+
+}

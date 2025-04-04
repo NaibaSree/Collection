5a1e131fab6f5849961bb6c4343d03f9e4422d94
Naiba Sree P <112084996+NaibaSree@users.noreply.github.com>
2025-04-04 14:39:42 +0530

    Create Student1.java

---
+++ Student1.java
@@ -0,0 +1,11 @@
+package Collection;
+
+public class Student1 {
+	String name;
+    int id,age;
+    Student1(String name, int id,int age){
+        this.name=name;
+        this.id=id;
+        this.age=age;
+    }
+}

45aefa4bb316fa7fcf19b2fe2dd334c10825ff05
Naiba Sree P <112084996+NaibaSree@users.noreply.github.com>
2025-04-04 14:41:08 +0530

    Create vector_main.java

---
+++ vector_main.java
@@ -0,0 +1,16 @@
+package Collection;
+import java.util.*;
+public class vector_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> vec=new Vector<String>();
+		vec.add("Java");
+		vec.add("database");
+		vec.add("Git");
+		System.out.println(vec);
+		vec.remove("database");
+		System.out.println("After removing : "+vec);
+	}
+
+}

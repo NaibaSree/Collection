97ed9960f6911ccb798b9a5a296d35e789e5d4fa
Naiba Sree P <112084996+NaibaSree@users.noreply.github.com>
2025-04-04 14:36:05 +0530

    Create List_Methods.java

---
+++ List_Methods.java
@@ -0,0 +1,16 @@
+package Collection;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class List_Methods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] num= {5,6,33,21,8};
+		
+	}
+
+}
